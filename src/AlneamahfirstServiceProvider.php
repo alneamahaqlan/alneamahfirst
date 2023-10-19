588b13c62d2e1fbe00f69ba6db5dcb002549a5c5
Aqlan Al-neamah <57961876+alneamahaqlan@users.noreply.github.com>
2023-10-19 23:00:02 +0300

    last

--- src/AlneamahfirstServiceProvider.php
+++ src/AlneamahfirstServiceProvider.php
@@ -8,6 +8,6 @@ class AlneamahfirstServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package) : void
     {
-        $package->name('alneamahfirst')->hasConfigFile();
+        $package->name('alneamahfirst')->hasConfigFile(['alneamahfirst']);
     }
 }
